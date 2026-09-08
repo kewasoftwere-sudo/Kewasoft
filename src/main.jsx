f8fad5f51477939c4e37d7abda9cbee97aa20ef3
kewasoftwere-sudo <kewasoftwere@gmail.com>
2026-09-08 19:45:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './Login.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
